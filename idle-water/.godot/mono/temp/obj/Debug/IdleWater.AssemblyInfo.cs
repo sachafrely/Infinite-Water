30c5d415ed1d561d9f97da9a2262c04b8ba3b048
sachafrely <sacha.frely@gmail.com>
2026-08-12 09:27:02 +0200

    Working on Environment. FPS got better for some reason.

--- idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9195f35fc44cc81df6886df16362cb9b184da52c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89201463ea61df37d25934ddbfce48d546dd9aed")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
