c094f853111bc5295a513c9d8a4b53e8bc74ea92
sachafrely <sacha.frely@gmail.com>
2026-08-13 00:25:59 +0200

    Working on Dynamic Rain

--- idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061fcdbb3290defe7be0bcc3a9d4b5bd4f13b186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10110b34fdf8580fcbf1563be03d93dcd0fb4e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
