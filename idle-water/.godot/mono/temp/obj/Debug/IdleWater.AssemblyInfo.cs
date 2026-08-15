b5d39ccd3b357d8881723c0041d75c78032c3109
sachafrely <sacha.frely@gmail.com>
2026-08-15 17:00:36 +0200

    PBF Refracturing Tested and works fine

--- idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8203405727e148702fe66bd20168951d09135fed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4d6aa3a6beea658661b5f149e1b29cdba4069a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
