f9232214fcb10e7900029ff4e6ca387682606bdb
sachafrely <sacha.frely@gmail.com>
2026-08-16 22:22:43 +0200

    Working On Window Sizes And Settings Panel

--- idle-water/.godot/mono/temp/obj/ExportDebug/android-arm64/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/ExportDebug/android-arm64/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef56df30d263b723d8a339f1070cf9807aac6e82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+614bc2490baa25d959da74d506e87fc2dce5edec")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
