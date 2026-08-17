f6aa4563f80352f5d68ab3b6824ce9f920ae4ad7
sachafrely <sacha.frely@gmail.com>
2026-08-17 19:39:29 +0200

    Aligned Decorations Correctly

--- idle-water/.godot/mono/temp/obj/ExportDebug/android-arm64/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/ExportDebug/android-arm64/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e21d287a2fc0d741cf31aa449dbbde12153083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2795b413cc451c344d4f62cff0f51d460586030b")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
