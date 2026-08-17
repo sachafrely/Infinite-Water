2795b413cc451c344d4f62cff0f51d460586030b
sachafrely <sacha.frely@gmail.com>
2026-08-17 18:42:36 +0200

    Changed Tilemap Scaling so that the width fits more neatly

--- idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e21d287a2fc0d741cf31aa449dbbde12153083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa53e768abc33dfaccb54486f1b55f584f976d5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
