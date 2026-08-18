4c9214c0d235938e4c96a621552926ca7f90f5fd
sachafrely <sacha.frely@gmail.com>
2026-08-18 23:10:08 +0200

    Added Tilt Indicator Sprite to Project

--- idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e441587ad3ce7acbec3650b20f39bc65a17d491")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7283b0db86e7a51b29a4add60948222ac9ed13bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
