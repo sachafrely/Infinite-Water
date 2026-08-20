8dfd45ae5e42bc53578a645e2a176cb2df1aff35
sachafrely <sacha.frely@gmail.com>
2026-08-20 12:36:00 +0200

    Compiled

--- idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df07d57ec4de515ccbc29f76004de2b442f3efd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164bc6ea39f3e00ac9a6e68a3b6a3a7f743ffb45")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
