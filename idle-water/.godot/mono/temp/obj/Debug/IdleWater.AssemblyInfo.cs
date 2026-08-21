2d52dcd29e8886848386f53dcf80d65847dde589
sachafrely <sacha.frely@gmail.com>
2026-08-21 09:11:37 +0200

    Executed and Runs Fine

--- idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/Debug/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc71170d5799ed9a5b20af8a8f88a08be7269f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9698d83ca2863b8625515cbb0d97a8fc459ce94b")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- idle-water/.godot/mono/temp/obj/ExportDebug/android-arm64/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/ExportDebug/android-arm64/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e441587ad3ce7acbec3650b20f39bc65a17d491")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9698d83ca2863b8625515cbb0d97a8fc459ce94b")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
