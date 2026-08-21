9925a97319ccba5b2e3b20b6d657041bfa15c9da
sachafrely <sacha.frely@gmail.com>
2026-08-21 13:14:58 +0200

    Buy Wheel System Successfully Implemented

--- idle-water/.godot/mono/temp/obj/ExportDebug/android-arm64/IdleWater.AssemblyInfo.cs
+++ idle-water/.godot/mono/temp/obj/ExportDebug/android-arm64/IdleWater.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("ExportDebug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9698d83ca2863b8625515cbb0d97a8fc459ce94b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10e9380e57cfab6edb9cbbd4e7ec16fde40dffb")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdleWater")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
