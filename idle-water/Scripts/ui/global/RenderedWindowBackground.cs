558576a56416a8590a36ace424b2e8fef1589355
ALEX <sacha.frely@gmail.com>
2026-08-28 21:15:23 +0200

    Use explicit UiSettings window background in RenderedWindowBackground

--- idle-water/Scripts/ui/global/RenderedWindowBackground.cs
+++ idle-water/Scripts/ui/global/RenderedWindowBackground.cs
@@ -20,39 +20,15 @@ public partial class RenderedWindowBackground : Control
 		if (Size.X <= 0.0f || Size.Y <= 0.0f)
 			return;
 
-		DrawRect(
-			new Rect2(Vector2.Zero, Size),
-			UiSettings.WindowColor,
-			true
-		);
+		DrawRect(new Rect2(Vector2.Zero, Size), UiSettings.WindowBackgroundColor, true);
 
 		float w = UiSettings.BorderSize;
-
 		if (w <= 0.0f)
 			return;
 
-		DrawRect(
-			new Rect2(0, 0, Size.X, w),
-			UiSettings.BorderColor,
-			true
-		);
-
-		DrawRect(
-			new Rect2(0, Size.Y - w, Size.X, w),
-			UiSettings.BorderColor,
-			true
-		);
-
-		DrawRect(
-			new Rect2(0, 0, w, Size.Y),
-			UiSettings.BorderColor,
-			true
-		);
-
-		DrawRect(
-			new Rect2(Size.X - w, 0, w, Size.Y),
-			UiSettings.BorderColor,
-			true
-		);
+		DrawRect(new Rect2(0, 0, Size.X, w), UiSettings.BorderColor, true);
+		DrawRect(new Rect2(0, Size.Y - w, Size.X, w), UiSettings.BorderColor, true);
+		DrawRect(new Rect2(0, 0, w, Size.Y), UiSettings.BorderColor, true);
+		DrawRect(new Rect2(Size.X - w, 0, w, Size.Y), UiSettings.BorderColor, true);
 	}
 }
