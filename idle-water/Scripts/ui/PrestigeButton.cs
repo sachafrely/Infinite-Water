29bc399e00c177057e62ba5663c141528f164676
ALEX <sacha.frely@gmail.com>
2026-08-29 09:42:01 +0200

    Keep Prestige button disabled until Prestige is implemented

--- idle-water/Scripts/ui/PrestigeButton.cs
+++ idle-water/Scripts/ui/PrestigeButton.cs
@@ -1,15 +1,13 @@
 using Godot;
-using System;
 
-public partial class PrestigeButton : Node
+/// <summary>
+/// Placeholder for the future Prestige system.
+/// Prestige remains disabled until the feature is implemented.
+/// </summary>
+public partial class PrestigeButton : Button
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Ready()
+    {
+        Disabled = true;
+    }
 }
