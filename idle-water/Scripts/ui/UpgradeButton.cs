e51bd5256e8698a1d6bbfeaf6244e4d9f2a17b7d
ALEX <sacha.frely@gmail.com>
2026-08-31 19:50:19 +0200

    Style wheel upgrade button with shared UI settings

--- idle-water/Scripts/ui/UpgradeButton.cs
+++ idle-water/Scripts/ui/UpgradeButton.cs
@@ -8,6 +8,8 @@ public partial class UpgradeButton : Button
 	public override void _Ready()
 	{
 		FocusMode = FocusModeEnum.None;
+		RenderedButtonBackground.Apply(this);
+		AddThemeFontSizeOverride("font_size", UiSettings.FontSizeMedium);
 		Pressed += OnPressed;
 	}
 
